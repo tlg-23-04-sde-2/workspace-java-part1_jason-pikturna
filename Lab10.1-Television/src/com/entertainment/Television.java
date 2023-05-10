fec28e6aa03ed7fef53ce8a84ffdbd3561bb702a
Jason Pikturna <Jason.Pikturna@tlgcohort.com>
2023-05-10 12:53:12 -0700

    Lab10.1-Television - practice on the throwing side of Exceptions

---
+++ Lab10.1-Television/src/com/entertainment/Television.java
@@ -0,0 +1,188 @@
+package com.entertainment;
+
+import java.util.Arrays;
+
+/*
+ *Business class (system class) to model the workings of a television
+ */
+public class Television {
+    // class-level, shared (static) variables - one copy of each, NOT stored in each object
+    public static final int MIN_VOLUME = 0;
+    public static final int MAX_VOLUME = 100;
+    public static final String[] VALID_BRANDS = { "Samsung", "LG", "Sony", "Toshiba" };
+
+    public static int instanceCount = 0;
+
+    public static int getInstanceCount() {
+        return instanceCount;
+    }
+
+    private static boolean isValidBrand(String brand) {
+        boolean valid = false;
+        for(String validBrand : VALID_BRANDS) {
+            if(validBrand.equals(brand)){
+                valid = true;
+                break;
+            }
+        }
+        return valid;
+    }
+
+
+
+
+    // instance variables or fields (properties or attributes)
+    private String brand = "Toshiba"; // default value when the client doesn't say; defaults to null when undefined
+    private int volume;// default value when the client doesn't say;  defaults to 0 without set value
+    private DisplayType display = DisplayType.LED;  // otherwise, null if client doesn't say
+
+    private boolean isMuted;    // for muting behaviour
+    private int oldVolume;      // for muting behaviour, internal use only, NO get/set methods
+
+    //constructors
+
+    public Television(){
+        boolean isConncected = verifyInternetConnection();
+        instanceCount++;
+    }
+
+    public Television(String brand) {
+        this();                 // delegate to no-arg ctor for the increment
+        setBrand(brand);        // delegate to its setter for any validation/conversion
+    }
+
+    public Television(String brand, int volume) throws InvalidVolumeException{
+        this(brand);            // delegate to ctor above me for brand
+        setVolume(volume);      // handle volume myself, by delegating to its setter
+    }
+
+    public Television(String brand, int volume, DisplayType display) throws InvalidVolumeException {
+        this(brand, volume);
+        setDisplay(display);
+    }
+
+
+    // business methods or operations
+   public void turnOn() {
+       boolean isConnected = verifyInternetConnection();
+       System.out.println("Turning on your " + getBrand() + " TV to volume " + getVolume());
+
+   }
+   public void turnOff(){
+        System.out.println("Shutting down...goodbye");
+    }
+
+
+    //mute is a boolean getter in this case
+    public void mute() {
+        if (!isMuted()) {           //the ! says NOT; as in NOT MUTED
+            oldVolume = getVolume();
+            volume = 0;
+            isMuted = true;
+        }
+        else {
+            volume = oldVolume;
+            isMuted = false;
+        }
+    }
+    //mute and unmute functions
+    /*
+     * will need two functions mute and unmute
+     * These will need access to volume
+     * Need a new variable to store the volume to revert to "currentVolume"
+     *
+     * Mute will set volume to 0
+     * unmute will set volume to previous volume
+     */
+
+
+
+
+
+    public String getBrand() {
+        return brand;
+    }
+
+
+    /* VALID_BRANDS looks like this:  ["Samsung", "LG", "Sony", "Toshiba"]
+     *
+     * for (String validBrand : VALID_BRANDS) {
+     *      // check the incoming brand against validBrand using the equals() method
+     * }
+     *      // if/when you find a match, you can terminate the loop early
+     *      // for example, if 'brand' comes in as "Sony"
+     */
+
+    public void setBrand(String brand) {
+        if (isValidBrand(brand)) {
+
+            this.brand = brand;
+        }
+        else {
+            System.out.printf("Invalid brand: %s.  Valid brands are %s.\n",
+           brand, Arrays.toString(VALID_BRANDS));
+        }
+
+    }
+
+    //switch practice.  This will (ALLEGEDLY) produce the same result as the above if/else for setBrand
+    //cases can be listed vertically, but MUST have a "break;" article between
+
+//    public void setBrand (String brand) {
+//        switch (brand) {
+//            case "Samsung": case "LG": case "Sony": case "Toshiba":
+//                this.brand = brand;
+//                break;
+//            default:
+//                System.out.println("Invalid brand:  " + brand + "." +
+//                        "Valid brands are:  Samsung, LG, Sony, Toshiba.");
+//        }
+//    }
+
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) throws InvalidVolumeException {
+        if (volume >= MIN_VOLUME && volume <= MAX_VOLUME) {  // valid
+            this.volume = volume;
+            // clear the "isMuted" flag
+            isMuted = false;
+        }
+        else {
+            throw new InvalidVolumeException(String.format(
+                    "Invalid volume:  %s.  Valid range: %s to %s.", volume, MIN_VOLUME, MAX_VOLUME
+            ));
+//            throw new IllegalArgumentException("Invalid Volume:  " + volume +
+//                    ".  Must be between " + MIN_VOLUME + " and " + MAX_VOLUME + "." );
+        }
+
+    }
+
+
+
+    public DisplayType getDisplay() {
+        return display;
+    }
+
+    public void setDisplay(DisplayType display) {
+        this.display = display;
+    }
+
+
+
+    public boolean isMuted() {
+        return isMuted;
+    }
+
+    private boolean verifyInternetConnection() {
+       return true;
+    }
+
+    public String toString() {
+        String volumeString = isMuted() ? "<muted>" : String.valueOf(getVolume());
+        return "Television: brand = " + getBrand() +
+                ", display = " + getDisplay() +  ", volume = " + volumeString;
+    }
+}
